d70f3908664799bd1f5ae0021c7e616a976441a7
KanCh <hagiabao1958@gmail.com>
2024-11-11 16:46:09 +0700

    refactor html css of main screen to bootstrap

--- button.js
+++ button.js
@@ -1,13 +1,4 @@
 document.querySelectorAll(".sidebar-button").forEach(button => {
-    var glow = button.querySelector(".sidebar-button-glow")
-    button.onmouseover = () => {
-        glow.style.opacity = "1"
-        glow.style.transform = "scale(1)"
-    }
-    button.onmouseleave = () => {
-        glow.style.opacity = "0"
-        glow.style.transform = "scale(0.85)"
-    }
     button.onmousedown = () => {
         button.style.transform = "scale(0.9)"
     }
