c055cd56946b01051511ab6b554ed55da8994c94
KanCh <hagiabao1958@gmail.com>
2024-11-14 22:56:27 +0700

    update button.js a bit

--- button.js
+++ button.js
@@ -1,13 +1,19 @@
 document.querySelectorAll(".sidebar-button").forEach(button => {
     var icon = button.querySelector("i")
-    button.onmousedown = () => {
+
+    button.addEventListener('mousedown', () => {
         icon.className = icon.className.replace(/\bbi-([\w+]+)\b/, (match, p1) => `bi-${p1}-fill`)
         button.style.transform = "scale(0.9)"
-    }
-    button.onmouseup = () => {
+        console.log('d')
+    })  
+
+    mouseout = () => {
         icon.className = icon.className.replace(/\bbi-([\w+]+)-fill\b/, (match, p1) => `bi-${p1}`)
         button.style.transform = "scale(1)"
     }
+
+    button.addEventListener('mouseup', mouseout)
+    button.addEventListener('mouseleave', mouseout)
 })
 
 document.querySelectorAll("#mobile-navbar div").forEach(button => {
