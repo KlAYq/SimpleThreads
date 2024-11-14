7fb56759080c50794ae08b4df6ae6893eef2424c
KanCh <hagiabao1958@gmail.com>
2024-11-14 22:42:22 +0700

    adjust button.js, add icon to sidebar, navbar

--- button.js
+++ button.js
@@ -1,23 +1,23 @@
 document.querySelectorAll(".sidebar-button").forEach(button => {
     var icon = button.querySelector("i")
     button.onmousedown = () => {
-        button.style.transform = "scale(0.9)"
         icon.className = icon.className.replace(/\bbi-([\w+]+)\b/, (match, p1) => `bi-${p1}-fill`)
+        button.style.transform = "scale(0.9)"
     }
     button.onmouseup = () => {
-        button.style.transform = "scale(1)"
         icon.className = icon.className.replace(/\bbi-([\w+]+)-fill\b/, (match, p1) => `bi-${p1}`)
+        button.style.transform = "scale(1)"
     }
 })
 
 document.querySelectorAll("#mobile-navbar div").forEach(button => {
     var icon = button.querySelector("i")
     button.onmousedown = () => {
-        button.style.transform = "scale(0.9)"
         icon.className = icon.className.replace(/\bbi-([\w+]+)\b/, (match, p1) => `bi-${p1}-fill`)
+        button.style.transform = "scale(0.9)"
     }
     button.onmouseup = () => {
-        button.style.transform = "scale(1)"
         icon.className = icon.className.replace(/\bbi-([\w+]+)-fill\b/, (match, p1) => `bi-${p1}`)
+        button.style.transform = "scale(1)"
     }
 })
