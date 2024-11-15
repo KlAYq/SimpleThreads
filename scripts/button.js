b9984fc32dd96415bd64517c6c7fc176f1300436
KanCh <hagiabao1958@gmail.com>
2024-11-15 07:59:01 +0700

    update a bit of js

--- scripts/button.js
+++ scripts/button.js
@@ -2,13 +2,13 @@ document.querySelectorAll(".sidebar-button").forEach(button => {
     var icon = button.querySelector("i")
 
     button.addEventListener('mousedown', () => {
-        icon.className = icon.className.replace(/\bbi-([\w+]+)\b/, (match, p1) => `bi-${p1}-fill`)
+        icon.className = icon.className.replace(/\bbi-([\w+\-]+)\b/, (match, p1) => `bi-${p1}-fill`)
         button.style.transform = "scale(0.9)"
-        console.log('d')
+        
     })  
 
     mouseout = () => {
-        icon.className = icon.className.replace(/\bbi-([\w+]+)-fill\b/, (match, p1) => `bi-${p1}`)
+        icon.className = icon.className.replace(/\bbi-([\w+\-]+)-fill\b/, (match, p1) => `bi-${p1}`)
         button.style.transform = "scale(1)"
     }
 
@@ -19,11 +19,15 @@ document.querySelectorAll(".sidebar-button").forEach(button => {
 document.querySelectorAll("#mobile-navbar div").forEach(button => {
     var icon = button.querySelector("i")
     button.onmousedown = () => {
-        icon.className = icon.className.replace(/\bbi-([\w+]+)\b/, (match, p1) => `bi-${p1}-fill`)
+        icon.className = icon.className.replace(/\bbi-([\w+\-]+)\b/, (match, p1) => `bi-${p1}-fill`)
         button.style.transform = "scale(0.9)"
     }
-    button.onmouseup = () => {
-        icon.className = icon.className.replace(/\bbi-([\w+]+)-fill\b/, (match, p1) => `bi-${p1}`)
+    
+    mouseout = () => {
+        icon.className = icon.className.replace(/\bbi-([\w+\-]+)-fill\b/, (match, p1) => `bi-${p1}`)
         button.style.transform = "scale(1)"
     }
+
+    button.addEventListener('mouseup', mouseout)
+    button.addEventListener('mouseleave', mouseout)
 })
