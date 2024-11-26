74f3ca00495c3957c591886f6cd01f146e01a231
KanCh <hagiabao1958@gmail.com>
2024-11-26 17:36:13 +0700

    change run script, convert some static screen to handlebars, add helper function to determine class icon
    - "start" script i package.json
    - getIcon helper function for desktop sidebar and mobile navbar
    - convert notifications, create post, my profile screen to handlebars

--- src/app.js
+++ src/app.js
@@ -11,7 +11,18 @@ app.engine(
         layoutsDir: __dirname + "/views/layouts",
         partialsDir: __dirname + "/views/partials",
         extname: "hbs",
-        defaultLayout: "layout"
+        defaultLayout: "layout",
+        helpers: {
+            getIcon: (page, icon) => {
+                const icons = new Map([
+                    ["home", ["bi-house", "bi-house-fill"]],
+                    ["create-post", ["bi-plus-square", "bi-plus-square-fill"]],
+                    ["notifications", ["bi-bell", "bi-bell-fill"]],
+                    ["my-profile", ["bi-person", "bi-person-fill"]]
+                ]);
+                return icons.get(icon)[icon == page ? 1 : 0]
+            }
+        }
     })
 )
 
@@ -22,7 +33,23 @@ app.get("/", (req, res) => {
 })
 
 app.get("/home", (req, res) => {
+    res.locals.page = "home"
     res.render("home")
+})
+
+app.get("/create-post", (req, res) => {
+    res.locals.page = "create-post"
+    res.render("create-post")
+})
+
+app.get("/notifications", (req, res) => {
+    res.locals.page = "notifications"
+    res.render("notifications")
+})
+
+app.get("/my-profile", (req, res) => {
+    res.locals.page = "my-profile"
+    res.render("my-profile")
 })
 
 app.listen(port, () => console.log(`Simple Threads starting.... port: ${port}`))
