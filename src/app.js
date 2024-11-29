cb80b471db17d6e2c2368f94b7cb73e6eee6f1fb
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-11-29 16:02:57 +0700

    setting up for auth action
    - auth session
    - auth password hashing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
+// if (process.env.NODE_ENV != 'production'){
+//   require('dotenv').config();
+// }
+
 const express = require('express')
 const viewEngine = require('express-handlebars')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
 const app = express()
 const port = 4000
 const { posts } = require('./public/temp/posts')
+const initPpt = require('./passport-config')
 
-app.use(express.static(__dirname + "/public"))
+initPpt(passport,
+  // Replace with real database
+    email => {users.find(user => user.email === email)},
+    id => {users.find(user => user.id === id)}
+)
+
+// Replace with real database
+const users = [];
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: false}));
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.engine(
     "hbs",
@@ -65,6 +95,60 @@ app.get("/post_view/:id", (req, res) => {
     res.locals.page = "post_view"
     res.render("post_view");
 })
+
+
+// Auth
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.post("/login", passport.authenticate('local', {
+  successRedirect: '/home',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch (e){
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut();
+  req.redirect('/login');
+})
+
+function checkAuthenticated(req, res, next){
+  if (req.isAuthenticated()){
+    return next();
+  }
+
+  res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next){
+  if (!req.isAuthenticated()){
+    return res.redirect('/home');
+  }
+
+  next();
+}
 
 
 app.listen(port, () => console.log(`Simple Threads starting.... port: ${port}`))
