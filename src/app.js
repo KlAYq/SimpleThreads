47d854b3b780f2ac5a9bd9b93d105c513e93ccf5
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-11-30 13:25:56 +0700

    session accquired

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-// if (process.env.NODE_ENV != 'production'){
-//   require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 
 const express = require('express')
 const viewEngine = require('express-handlebars')
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
-// const session = require('express-session')
+const session = require('express-session')
 const methodOverride = require('method-override')
 
 const app = express()
@@ -29,14 +29,18 @@ initPpt(passport,
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({extended: false}));
-// app.use(flash())
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: false,
-// }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
 app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
 app.use(methodOverride('_method'))
 
 app.engine(
