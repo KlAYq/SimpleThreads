3e560c6cab493621de20269ec037b7a83b286f5f
Le Tri Man <120546650+KlAYq@users.noreply.github.com>
2024-12-03 12:59:34 +0700

    proper register/login system
    now register and login function can be stored and verify with actual database

--- src/passport-config.js
+++ src/passport-config.js
@@ -1,32 +1,41 @@
-const LocalStrategy = require('passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
-function initPassport(passport, getUserByUsername, getUserById){
+function initPassport(passport, getUserByUsername, getUserById) {
   const authenticateUser = async (username, password, done) => {
-    const user = getUserByUsername(username);
-    if (user == null){
-      return done(null, false, {message: 'No user'});
+    const user = await getUserByUsername(username);
+    
+    if (user == null) {
+      console.log("No user with that username")
+      return done(null, false);
     }
 
-
     try {
-      if (await bcrypt.compare(password, user.password)){
+      if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
-      }
-      else {
-        return done(null, false, {message : 'Wrong password.'});
+      } else {
+        console.log("Incorrect password")
+        return done(null, false);
       }
     } catch (e) {
       return done(e);
     }
-  }
+  };
 
-  passport.use(new LocalStrategy({usernameField: 'username'},  authenticateUser));
-  passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((id, done) => {
-      return done(null, getUserById(id))
+  passport.use(new LocalStrategy({ usernameField: 'username' }, authenticateUser));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await getUserById(id);
+      done(null, user);
+    } catch (e) {
+      done(e);
+    }
   });
 }
 
-
-module.exports = initPassport
+module.exports = initPassport;
