8fd6e02414a9a6a202c3a640d4695a4e722efcd8
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-12-23 18:58:28 +0700

    controllers and extra notification class

---
+++ src/models/notification.js
@@ -0,0 +1,23 @@
+"use strict";
+const { Model } = require("sequelize");
+const {options} = require("pg/lib/defaults");
+module.exports = (sequelize, DataTypes) => {
+  class Notification extends Model {
+    static associate(models){
+      Notification.belongsTo(models.User, {foreignKey: "userId"});
+      Notification.belongsTo(models.Post, {foreignKey: "postId"});
+    }
+  }
+
+  Notification.init(
+    {
+      content: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Notification",
+    }
+  );
+
+  return Notification;
+}
